2761fb6db6bedc845e02c36d8c9f94784fd4db94
sharkz <mosenjemudogoz@gmail.com>
2024-02-17 05:47:00 +0300

    store products

--- app/Http/Controllers/AdminProductsController.php
+++ app/Http/Controllers/AdminProductsController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 
 class AdminProductsController extends Controller
@@ -28,7 +30,34 @@ class AdminProductsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => 'required|string',
+            'price' => 'required|numeric',
+            'category' => 'required|string',
+            'description' => 'required|string',
+            'quantity' => 'required|integer',
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048', // validate image file
+        ]);
+    
+        $imageName = $request->file('image')->store('public/images'); // store the image in the storage directory
+    
+        $user = Auth::user(); // Retrieve the authenticated user
+
+        $product = new Product([
+            'name' => $request->input('name'),
+            'price' => $request->input('price'),
+            'category' => $request->input('category'),
+            'description' => $request->input('description'),
+            'quantity' => $request->input('quantity'),
+            'image' => $imageName,
+            'user_id' => $user->id, // Set the user_id field
+        ]);
+
+
+    
+        $product->save();
+    
+        return redirect()->route('products.index')->with('success', 'Product added successfully.');
     }
 
     /**

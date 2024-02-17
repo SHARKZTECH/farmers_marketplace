d8735859f42fd69e8d627b4b2bc2287f2ed84693
sharkz <mosenjemudogoz@gmail.com>
2024-02-17 05:09:45 +0300

    product model and migrations

---
+++ app/Models/Product.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'price',
+        'category',
+        'image',
+        'description',
+        'quantity', 
+    ];
+
+    /**
+     * Get the user that owns the product.
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}

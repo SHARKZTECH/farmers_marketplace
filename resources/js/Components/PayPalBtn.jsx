87f3b7554329fdc84b40a8289b064464de295805
sharkz <mosenjemudogoz@gmail.com>
2024-03-07 14:23:57 +0300

    paypalbtn fxs

--- resources/js/Components/PayPalBtn.jsx
+++ resources/js/Components/PayPalBtn.jsx
@@ -49,7 +49,7 @@ const PayPalBtn = ({amount}) => {
     return (
         <div style={{ maxWidth: "750px", minHeight: "200px" }}>
             <PayPalScriptProvider options={{ clientId: "AbZDN6soec8-E-qH0B7bt4SaX6L7K_TIUJrxC7nRj6nXW05l4kJE6fLIQAV_50yShhxOom6g6Tx_dgLd", components: "buttons", currency: "USD" }}>
-                <ButtonWrapper showSpinner={false} amount={amount}/>
+                <ButtonWrapper showSpinner={false} amount={(Number(amount) / 140).toFixed(2)}/>
             </PayPalScriptProvider>
         </div>
     );
